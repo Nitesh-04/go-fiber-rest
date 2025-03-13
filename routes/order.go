956b8e386c2b7c2d8db765a620b8a32ecbdca2ee
nitesh kakkar <kakkar.nitesh04@gmail.com>
2025-03-13 17:16:39 +0530

    ft: get order and update

--- routes/order.go
+++ routes/order.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"errors"
 	"time"
 
 	"github.com/Nitesh-04/go-fiber-rest/database"
@@ -24,7 +25,7 @@ func CreateResponseOrder(order models.Order, user User, product Product) Order {
 	}
 }
 
-func CreateOrder (c *fiber.Ctx) error {
+func CreateOrder(c *fiber.Ctx) error {
 	var order models.Order
 
 	if err := c.BodyParser(&order); err!=nil {
@@ -54,7 +55,7 @@ func CreateOrder (c *fiber.Ctx) error {
 
 }
 
-func GetOrders (c *fiber.Ctx) error {
+func GetOrders(c *fiber.Ctx) error {
 	orders := []models.Order{}
 
 	database.Database.Db.Find(&orders)
@@ -74,4 +75,68 @@ func GetOrders (c *fiber.Ctx) error {
 	}
 
 	return c.Status(200).JSON(responseOrders)
+}
+
+func FindOrder(id int, order *models.Order) error {
+	database.Database.Db.Find(&order, "id = ?", id)
+	if order.ID == 0 {
+		return errors.New("No order exists")
+	}
+
+	return nil
+}
+
+func GetOrderById(c *fiber.Ctx) error {
+	id,err := c.ParamsInt("id")
+
+	var order = models.Order{}
+
+	if err != nil {
+		return c.Status(400).JSON("Order not found")
+	}
+
+	if err := FindOrder(id, &order); err != nil {
+		return c.Status(400).JSON("Order not found")
+	}
+
+	var user models.User
+	var product models.Product
+
+	database.Database.Db.First(&user, "id = ?", order.UserID)
+	database.Database.Db.First(&product, "id = ?", order.ProductID)
+
+	responseOrder := CreateResponseOrder(order,CreateResponseUser(user),CreateResponseProduct(product))
+
+	return c.Status(200).JSON(responseOrder)
+}
+
+func UpdateOrder(c *fiber.Ctx) error {
+	orderId, err := c.ParamsInt("id")
+
+	var order = models.Order{}
+
+	if err != nil {
+		return c.Status(400).JSON("Order not found")
+	}
+
+	if database.Database.Db.Find(&order, "id = ?", orderId); order.ID == 0 {
+		return c.Status(400).JSON("Order not found")
+	}
+
+	var newOrder models.Order
+
+	if err := c.BodyParser(&newOrder); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	if err := FindUser(int(newOrder.UserID), &models.User{}); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	if err := FindProduct(int(newOrder.ProductID), &models.Product{}); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	database.Database.Db.Model(&order).Updates(newOrder)
+
 }
